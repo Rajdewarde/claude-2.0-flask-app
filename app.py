d4123522bc3edb834d2ee2a34a535ddc456438f2
Raj Dewarde <rajdewarde7777@gmail.com>
2026-08-04 22:47:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,4 +84,5 @@ def chat_stream():
     return Response(stream_with_context(generate()), content_type='text/event-stream')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
